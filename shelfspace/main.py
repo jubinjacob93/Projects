d98ac3c1f0c1d1c01b50475688217cce5d7b8133
Jubin Jacob Varughese <vjubin@hotmail.com>
2026-07-02 14:05:08 -0400

    Adding few more endpoints.

--- shelfspace/main.py
+++ shelfspace/main.py
@@ -7,8 +7,14 @@ app=FastAPI()
 class BookCreate(BaseModel):
     title: str=Field(min_length=3)
     author: str=Field(min_length=2)
-    rating: int=Field(gt=0, le=5)
+    rating: Optional[int]=Field(default=None, ge=1, le=5)
     is_read: bool=Field(default=False)
+
+class BookUpdate(BaseModel):
+    title: Optional[str]=Field(min_length=3)
+    author: Optional[str]=Field(min_length=2)
+    rating: Optional[int]=Field(gt=0, le=5)
+    is_read: Optional[bool]=Field(default=None)
 
 class Book(BookCreate):
     id: int=Field(gt=0)
@@ -22,7 +28,7 @@ def create_book(book: BookCreate):
     return new_book
 
 @app.get("/books/")
-def get_book(is_read: Optional[bool]=None):
+def get_books(is_read: Optional[bool]=None):
     if is_read is None:
         return books
     return [
@@ -34,6 +40,25 @@ def get_one_book(book_id:int):
     for bk in books:
         if bk.id==book_id:
             return bk
-    raise HTTPException(statuscode=404, detail="Book ID not found") 
+    raise HTTPException(status_code=404, detail="Book ID not found") 
 
-app.
+@app.patch("/books/{book_id}")
+def update_book(book_id:int, book:BookUpdate):
+    updated_book=book.model_dump(exclude_unset=True)
+    for bk in books:
+        if bk.id==book_id:
+            if bk.rating is None and book.rating is None and book.is_read is True:
+                raise HTTPException(status_code=400, detail="Finished books need to have a rating")
+            for key, value in updated_book.items():
+                setattr(bk, key, value)
+            return bk
+    raise HTTPException(status_code=404, detail="Book ID not found")
+
+
+@app.delete("/books/{book_id}")
+def delete_book(book_id: int):
+    for ix, bk in enumerate(books):
+        if bk.id==book_id:
+            books.pop(ix)
+            return {"message": "Book deleted"}
+    raise HTTPException(status_code=404, detail="Book ID not found")
